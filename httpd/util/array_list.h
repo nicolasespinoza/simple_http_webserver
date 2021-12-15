3bc4c80a712c35587c495d8ea015efcec0102fc6
BuildTools <unconfigured@null.spigotmc.org>
2021-12-15 12:03:03 -0800

    Fixed incorrect header file, done

--- httpd/util/array_list.h
+++ httpd/util/array_list.h
@@ -10,4 +10,4 @@ struct arraylist {
 struct arraylist* array_list_new(size_t data_type_size);
 void array_list_add_to_end(struct arraylist* list, void* item);
 void* array_list_get_item(struct arraylist* list, int index);
-void array_list_cleanup();
+void array_list_cleanup(struct arraylist* list);
