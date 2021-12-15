2a9893e1b90be81d3c2b80062247d851c7941599
BuildTools <unconfigured@null.spigotmc.org>
2021-12-14 21:19:22 -0800

    Fixed curl hanging on error code(s)

--- httpd/build.sh
+++ httpd/build.sh
@@ -1,2 +1,1 @@
 gcc server.c net.c util/toolkit.c util/array_list.c -o out/server.out -std=c99
-gcc client.c net.c util/toolkit.c util/array_list.c -o out/client.out -std=c99
--- httpd/server.c
+++ httpd/server.c
@@ -14,7 +14,6 @@ void send_html_error(char* message) {
 
 void send_reply(int response_code, int content_length) {
     // TODO: BEFORE THE FINAL TEXT IS SENT, VERIFY CLIENT IS STILL CONNECTED
-    // TODO: uh maybe send all of these with an HTML casing, meaning <html><h1>ETC ETC</h1></html>
     if (response_code == 400) {
         send_html_error("HTTP/1.0 400 Bad Request");
     } else if (response_code == 403) {
@@ -34,7 +33,7 @@ void send_reply(int response_code, int content_length) {
 }
 
 void end_request_cleanup(FILE* file, struct arraylist* split_request_text, char* expected_valid_file_name,
-        int previous_std_out) {
+        int previous_std_out, int client_file_descriptor) {
     if (file != NULL) {
         fclose(file);
     }
@@ -43,15 +42,21 @@ void end_request_cleanup(FILE* file, struct arraylist* split_request_text, char*
     }
     array_list_cleanup(split_request_text);
 
+//    close(client_file_descriptor);
+
     // restore stdout file descriptors
     dup2(previous_std_out, 1);
     close(previous_std_out);
+
+//    exit(0); exiting just makes it to the connection never closes
 }
 
 void handle_client_request(char* client_request, int client_file_descriptor) {
     int previous_std_out = dup(1); // previous stdout
     dup2(client_file_descriptor, STDOUT_FILENO);
-    //close(client_file_descriptor); this closes the connection
+    close(client_file_descriptor); // this closes the connection
+
+//    printf("Client request: [%s]\n", client_request);
 
     struct arraylist* split_request_text = split(client_request, " ");
 
@@ -61,7 +66,7 @@ void handle_client_request(char* client_request, int client_file_descriptor) {
 
     if (split_request_text->number_of_items < 3) { // malformed request / invalid syntax
         send_reply(400, -1);
-        end_request_cleanup(NULL, split_request_text, NULL, client_file_descriptor);
+        end_request_cleanup(NULL, split_request_text, NULL, previous_std_out, client_file_descriptor);
         return;
     }
 
@@ -69,7 +74,7 @@ void handle_client_request(char* client_request, int client_file_descriptor) {
     if (split_request_text->number_of_items > 3 || // if syntax has more than 3 arguments
         (strcmp(request_type, "GET") != 0 && strcmp(request_type, "HEAD") != 0)) { // if request isn't HEAD or GET
         send_reply(501, -1);
-        end_request_cleanup(NULL, split_request_text, NULL, client_file_descriptor);
+        end_request_cleanup(NULL, split_request_text, NULL, previous_std_out, client_file_descriptor);
         return;
     }
 
@@ -77,7 +82,7 @@ void handle_client_request(char* client_request, int client_file_descriptor) {
     if (strcmp(expected_valid_file_name, "/") == 0 || // checks if only "/" is passed as the file name
             contains_double_dot(expected_valid_file_name)) { // checks if file name contains ".." to access invalid areas
         send_reply(400, -1);
-        end_request_cleanup(NULL, split_request_text, expected_valid_file_name, client_file_descriptor);
+        end_request_cleanup(NULL, split_request_text, expected_valid_file_name, previous_std_out, client_file_descriptor);
         return;
     }
 
@@ -97,7 +102,7 @@ void handle_client_request(char* client_request, int client_file_descriptor) {
         } else if (errno == EACCES) { // cannot access / no permissions
             send_reply(403, -1);
         }
-        end_request_cleanup(file, split_request_text, expected_valid_file_name, client_file_descriptor);
+        end_request_cleanup(file, split_request_text, expected_valid_file_name, previous_std_out, client_file_descriptor);
         return;
     }
 
@@ -106,9 +111,10 @@ void handle_client_request(char* client_request, int client_file_descriptor) {
 
     send_reply(200, file_size);
     if (is_get) {
-        print_file_contents(file); // remember to adapt this for HTML wrapping? (not sure)
+        print_file_contents(file);
+//        printf("\n");
     }
-    end_request_cleanup(file, split_request_text, expected_valid_file_name, client_file_descriptor);
+    end_request_cleanup(file, split_request_text, expected_valid_file_name, previous_std_out, client_file_descriptor);
 
 }
 
@@ -126,8 +132,17 @@ void handle_request(int nfd)
    }
 
    while ((num = getline(&line, &size, network)) >= 0) {
-       printf("QUERY RECEIVED | Handling...\n");
        handle_client_request(line, nfd);
+       close(nfd);
+       // close file descriptor for client?
+       //exit(0); // should exit after the first line is processed. All other lines will not be processed
+//            printf("Done\n");
+//            write(nfd, "done", 10);
+//            close(nfd);
+//            exit(0);
+//        }
+//       handle_client_request(line, nfd);
+
 //      write(nfd, line, size); // backup. origin
 //       write(nfd, "lol test", size);
    }
@@ -147,18 +162,26 @@ void run_service(int fd)
           if (childProcessStatus == -1) {
               perror("fork");
           } else if (childProcessStatus == 0) {
-              printf("Connection established\n");
+              int pid = getpid();
+              printf("Connection established (ID: %d)\n", pid);
               handle_request(nfd);
-              printf("Connection closed\n");
+
+              close(fd);
+              close(nfd);
+
+              printf("Connection closed (ID: %d)\n", pid);
+
               exit(0);
+          } else {
+              wait(0);
+
+              close(nfd);
           }
       }
    }
 }
 
 int main(int number_of_arguments, char* arguments[]) {
-//    handle_client_request("GET /test.txt HTTP/1.0");
-
     if (number_of_arguments == 2) {
         int port = string_to_int(arguments[1], 1);
         int fd = create_service(port);
